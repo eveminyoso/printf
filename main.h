8aeba0639c5d19b53d87b70eacd8382d87e9eda0
AlexDelei <alexdelei125@gmail.com>
2023-08-21 13:12:19 -0700

    String

--- main.h
+++ main.h
@@ -20,6 +20,21 @@ int _printstring(char *s);
 int rot13(char *point);
 int print_rev(char *);
 int print_hl(uintptr_t n);
-int print_bigS(va_list l, flags_t *f);
+int print_bigS(va_list l);
 int main(void);
+
+
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
 #endif
