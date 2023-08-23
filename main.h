b5ac0084504e5bbd67786a89173fcc80ca920e9e
eveminyoso <eveminyoso@gmail.com>
2023-08-23 15:52:10 -0700

    fixing

---
+++ main.h
@@ -0,0 +1,40 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int print_number(int n);
+int _printf(const char *format, ...);
+int print_octal(unsigned int n);
+int print_unsig(unsigned int n);
+int print_hexaup(unsigned int n);
+int print_hexalow(unsigned int n);
+int print_bi(unsigned int n);
+int _printstring(char *s);
+int rot13(char *point);
+int print_rev(char *s);
+int print_hl(unsigned int n);
+int print_S(char *s);
+int main(void);
+
+
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+#endif
